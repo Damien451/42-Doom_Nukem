296d2221f7ca3b2dfe16980ab5ec4286d90a060c
Eliott SUITS <esuits@e1r12p11.42.fr>
2019-10-25 19:18:48 +0200

    made changes in thunder sound

--- srcs/states/state_start.c
+++ srcs/states/state_start.c
@@ -119,11 +119,10 @@ static inline void	draw_on_texture(t_doom *data, unsigned int *image)
 		i++;
 	}
 	if (type > 0 && frame >= 120 && frame <= 135)
-	{
 		lightning(data, image, (((frame + 50) << 16) | ((frame + 50) << 8))
 			+ frame + 75, frame);
+	if (type >0 && frame == 120)
 		play_sound(data->mix->sounds[3]);
-	}
 	frame += type;
 	if (frame == 255 || frame == 0)
 		type *= -1;
