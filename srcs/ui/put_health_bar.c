8bb18f763b6d142bae82f3d54cb3f881c4796917
Eliott SUITS <esuits@e1r4p7.42.fr>
2019-12-09 19:04:27 +0100

    changed defines from .c to .h

--- srcs/ui/put_health_bar.c
+++ srcs/ui/put_health_bar.c
@@ -2,11 +2,6 @@
 #include "menus.h"
 #include "libft.h"
 
-# define HEALTH_BAR_HEIGHT_START 853
-# define HEALTH_BAR_WIDTH_START 584
-# define HEALTH_BAR_WIDTH 750
-# define HEALTH_BAR_HEIGHT 50
-
 void	put_health_bar(t_doom *data)
 {
 	draw_rectangle(&(data->lib), (t_point){HEALTH_BAR_WIDTH_START, HEALTH_BAR_HEIGHT_START},
